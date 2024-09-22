0eb389c5884628aeab4a11a3f81ea8db406c69a9
zixun <chenyl.exe@gmail.com>
2024-09-22 16:30:30 +0800

    add mj_refresh

--- GodEye/Classes/Main/Controller/Sub/ConsolePrintViewController.swift
+++ GodEye/Classes/Main/Controller/Sub/ConsolePrintViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-//import ESPullToRefresh
+import MJRefresh
 
 class ConsolePrintViewController: UIViewController {
     
@@ -40,18 +40,17 @@ class ConsolePrintViewController: UIViewController {
         
         self.view.addSubview(self.recordTableView)
         self.view.addSubview(self.inputField)
-        
-//        self.recordTableView.es.addPullToRefresh { [weak self] in
-//            guard let sself = self else {
-//                return
-//            }
-//            
-//            let result = sself.dataSource.loadPrePage()
-//            if result == true {
-//                sself.recordTableView.reloadData()
-//            }
-//            sself.recordTableView.es.stopPullToRefresh()
-//        }
+        self.recordTableView.mj_footer = MJRefreshAutoNormalFooter.init(refreshingBlock: { [weak self] in
+            guard let sself = self else {
+                return
+            }
+
+            let result = sself.dataSource.loadPrePage()
+            if result == true {
+                sself.recordTableView.reloadData()
+            }
+            sself.recordTableView.mj_footer?.endRefreshing()
+        })
         
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(ConsolePrintViewController.keyboardWillShow(noti:)),
